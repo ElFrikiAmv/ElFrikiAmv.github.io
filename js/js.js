49e38b0314c3504c16bc9426c9c1e056408a96e0
elfrikiamv <elfrikiamv@gmail.com>
2023-04-02 18:18:07 -0600

    save: add navigation bar, welcome section, skills section and footer section.

---
+++ js/js.js
@@ -0,0 +1,19 @@
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Initialize smooth scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 800,
+  speedAsDuration: true
+})
+
+// Initialize AOS
+AOS.init({
+    once: true
+  });
+
+  
+  
