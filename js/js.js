e36f0624d706b8a05c82d3ef5474f137c44d449e
elfrikiamv <elfrikiamv@gmail.com>
2023-10-18 01:46:13 -0600

    save: change background.

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 //  Para desplazar la página a la sección de contacto al hacer clic en el botón
 document
-  .getElementById("scrollToContact")
+  .getElementById("btnWelcomeScrollToContact")
   .addEventListener("click", function () {
     document
       .getElementById("contact-section")
